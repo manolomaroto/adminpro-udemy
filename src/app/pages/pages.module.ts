9fc4d678f0282a01ef80f22c25f945d90f3a74fc
Manuel Gonzalez Maroto <manuelgmaroto@gmail.com>
2019-05-19 14:22:20 +0200

    Seccion 8 terminada

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -10,6 +10,8 @@ import { IncrementadorComponent } from '../components/incrementador/incrementado
 
 import { GraficaDonaComponent } from '../components/grafica-dona/grafica-dona.component';
 import { AccountSettingsComponent } from './account-settings/account-settings.component';
+import { PromesasComponent } from './promesas/promesas.component';
+import { RxjsComponent } from './rxjs/rxjs.component';
 
 @NgModule({
     declarations: [
@@ -19,7 +21,9 @@ import { AccountSettingsComponent } from './account-settings/account-settings.co
         Graficas1Component,
         IncrementadorComponent,
         GraficaDonaComponent,
-        AccountSettingsComponent
+        AccountSettingsComponent,
+        PromesasComponent,
+        RxjsComponent
     ],
     exports: [
         DashboardComponent,
--- src/app/services/shared/sidebar.service.ts
+++ src/app/services/shared/sidebar.service.ts
@@ -19,6 +19,12 @@ export class SidebarService {
       }, {
         titulo: 'Graficas',
         url: '/graficas1'
+      }, {
+        titulo: 'Promesas',
+        url: '/promesas'
+      }, {
+        titulo: 'Rxjs',
+        url: '/rxjs'
       }
     ]
 
